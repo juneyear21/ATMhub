3a8763663f3b2b28eef5f63a74631ec68bf92da6
juneyear21 <88622903+juneyear21@users.noreply.github.com>
2024-02-29 18:14:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,56 @@
 function searchMovie() {
-    const searchQuery = document.querySelector('.searchInput').value;
-    const movieDatabaseAPI = 'https://api.themoviedb.org/3/search/movie';
-    const apiKey = '557c35bbbe97da53f3bdfa06f11ff7de';
+  const searchQuery = document.querySelector(".searchInput").value;
+  const movieDatabaseAPI = "https://api.themoviedb.org/3/search/movie";
+  const apiKey = "557c35bbbe97da53f3bdfa06f11ff7de";
 
-    const options = {
-      method: 'GET',
-      headers: {
-        'accept': 'application/json'
-      }
-    };
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+    },
+  };
 
-    fetch(`${movieDatabaseAPI}?query=${encodeURIComponent(searchQuery)}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`, options)
-      .then(response => response.json())
-      .then(data => {
-        displayMovies(data.results);
-      })
-      .catch(error => console.error('Error:', error));
-  }
+  fetch(
+    `${movieDatabaseAPI}?query=${encodeURIComponent(
+      searchQuery
+    )}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      displayMovies(data.results);
+    })
+    .catch((error) => console.error("Error:", error));
+}
 
-  function displayMovies(movies) {
-    const moviesContainer = document.getElementById('moviesContainer');
-    moviesContainer.innerHTML = '';
+function displayMovies(movies) {
+  const seriesFrame = document.getElementById("movieFrame");
+  seriesFrame.src = "";
+  const moviesContainer = document.getElementById("moviesContainer");
+  moviesContainer.innerHTML = "";
 
-    movies.forEach(movie => {
-      console.log(movie);
-      const card = `
-        <div class="movie-card">
-          <img class="movie-image" src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}" alt="${movie.original_title}">
-          <div class="movie-details">
-            <div class="movie-title">${movie.original_title}</div>
-            <div class="movie-rating">Rating: ${movie.vote_average}</div>
-            
-          </div>
-          <button class="watch-now-btn" onclick="watchMovie('${movie.id}')">Watch Now</button>
-        </div>
-      `;
-      moviesContainer.innerHTML += card;
-    });
-  }
+  movies.forEach((movie) => {
+    console.log(movie);
+    const card = `
+      <div class="movie-card">
+      <img class="movie-image" src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}" alt="${movie.original_title}">
+      <div class="movie-details">
+          <div class="movie-title">${movie.original_title}</div>
+          <div class="movie-rating">Rating: ${movie.vote_average}</div>
+          
+      </div>
+      <button class="watch-now-btn" onclick="watchMovie('${movie.id}')">Watch Now</button>
+      </div>
+  `;
+    moviesContainer.innerHTML += card;
+  });
+}
 
-  function watchMovie(movieId) {
-    const vidsrcUrl = `https://vidsrc.to/embed/movie/${movieId}`;
-    window.open(vidsrcUrl);
-  }
+function watchMovie(movieId) {
+  const moviesContainer = document.getElementById("moviesContainer");
+  moviesContainer.innerHTML = "";
+  const movieViewer = document.getElementById("movieViewer");
+  movieViewer.style.display = "block";
+  const movieFrame = document.getElementById("movieFrame");
+  movieFrame.src = `https://vidsrc.to/embed/movie/${movieId}`;
+}
